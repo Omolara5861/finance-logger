4352d719811dd1098e921989524d301418a806a4
Omolara5861 <debbiegterra@gmail.com>
2023-01-27 11:49:52 +0100

    feat: :sparkles: Create Invoice class

---
+++ src/classes/invoice.ts
@@ -0,0 +1,7 @@
+class Invoice implements HasFormatter {
+    constructor(readonly client: string, public details: string, private amount: number) {}
+
+    format(): string {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
