58fa57b33e24e059419a14895681094b398f2fb0
Omolara5861 <debbiegterra@gmail.com>
2023-01-29 21:09:42 +0100

    refactor: :recycle: Reformat list template file

--- src/classes/list-template.ts
+++ src/classes/list-template.ts
@@ -9,10 +9,11 @@ export class ListTemplate {
         let h4 = document.createElement('h4');
         let p = document.createElement('p');
 
-        h4.innerHTML = heading;
-        p.innerHTML = item.format();
+        h4.innerText = heading;
+        li.append(h4)
+        p.innerText = item.format();
 
-        li.append(h4, p);
+        li.append(p);
 
         if(position == 'start') this.container.prepend(li);
         else if(position == 'end') this.container.append(li);
